084e9255bd3d6a73a4d41074d1906aa55ab4b30b
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-07-27 15:55:46 +0100

    cleaned up methods, broke interactive menu into individual methods, made students an instance varibale

--- directory.rb
+++ directory.rb
@@ -1,20 +1,52 @@
+@students = [] #empty array accessible to all methods
+
+def interactive_menu
+  loop do
+    print_menu
+    #get user input
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "What would you like to do?"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, hit return/enter twice"
-
-  students = []
   # lets user input name of student
   name = gets.chomp
   # if name is not empty, will do this
   while !name.empty? do
     # add name with key name and november with key cohort to student hash
-    students << {name: name, cohort: :november}
+    @students << {name: name, cohort: :november}
     #counts the new total of students after one is added
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
+end
 
-  students
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -23,17 +55,13 @@ def print_header
 end
 
 #iterates through hash of students to print their names and cohort
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 #prints count of students in hash
-def print_footer(students)
-  puts "Overall we have #{students.count} great students"
+def print_footer
+  puts "Overall we have #{@students.count} great students"
 end
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
