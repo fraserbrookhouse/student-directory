e2bf26dfe660cdb33d77bd7ae2e5f234c213b7ea
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-07-27 16:08:23 +0100

    added ability to save directory to csv, added csv file to .gitignore

--- directory.rb
+++ directory.rb
@@ -12,6 +12,7 @@ def print_menu
   puts "What would you like to do?"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save directory to students.csv"
   puts "9. Exit"
 end
 
@@ -21,6 +22,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -64,4 +67,17 @@ end
 def print_footer
   puts "Overall we have #{@students.count} great students"
 end
+
+def save_students
+  #open file for writing
+  file = File.open("students.csv", "w")
+  #iterate over student array, extract each students data to a single string to write to csv
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 interactive_menu
